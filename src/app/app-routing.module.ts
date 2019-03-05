d6f9766f08d123c2052a3a00b83b67e7a2281322
Anthony Luc <anthonyvanluc@gmail.com>
2019-03-05 18:04:07 -0500

    add /fighters route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { FightersComponent }     from './fighters/fighters.component'
+
+const routes: Routes = [
+  { path: 'fighters', component: FightersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
