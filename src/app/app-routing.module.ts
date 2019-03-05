f4eb582cd3dae2970b372696b23109135f590923
Anthony Luc <anthonyvanluc@gmail.com>
2019-03-05 18:38:58 -0500

    move fighter detail to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule }               from '@angular/core';
+import { Routes, RouterModule }   from '@angular/router';
 
-import { DashboardComponent }   from './dashboard/dashboard.component';
-import { FightersComponent }    from './fighters/fighters.component'
+import { DashboardComponent }     from './dashboard/dashboard.component';
+import { FightersComponent }      from './fighters/fighters.component'
+import { FighterDetailComponent } from './fighter-detail/fighter-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: FighterDetailComponent },
   { path: 'fighters', component: FightersComponent }
 ];
 
